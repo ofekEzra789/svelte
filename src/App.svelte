51a85f5cebc54ee647b72e36b3bd027fbf54d592
ofek <ezra@gmail.com>
2020-10-24 09:15:29 +0300

    first commit svelte

---
+++ src/App.svelte
@@ -0,0 +1,32 @@
+<script>
+  import Header from "./components/Header.svelte";
+  import Footer from "./components/Footer.svelte";
+  import Tabs from "./shared/Tabs.svelte";
+  import CreatePollForm from './components/CreatePollForm.svelte'
+
+  // tabs
+  let items = ["Current Polls", "Add New Poll"];
+  let activeItem = "Current Polls";
+
+  const handleTabChange = (e) => {
+    activeItem = e.detail;
+  };
+</script>
+
+<style>
+  main {
+    max-width: 960px;
+    margin: 40px auto;
+  }
+</style>
+
+<Header />
+<main>
+  <Tabs on:tabChange={handleTabChange} {items} {activeItem} />
+  {#if activeItem === 'Current Polls'}
+    <p>Poll List Component goes here</p>
+  {:else if activeItem === 'Add New Poll'}
+    <CreatePollForm />
+  {/if}
+</main>
+<Footer />
---
+++ src/components/CreatePollForm.svelte
@@ -0,0 +1,94 @@
+<script>
+  import Button from "../shared/Button.svelte";
+  let fields = {
+    question: "",
+    answerA: "",
+    answerB: "",
+  };
+  let errors = {
+    question: "",
+    answerA: "",
+    answerB: "",
+  };
+
+  let valid = false;
+
+  const handleSubmit = () => {
+    valid = true;
+
+    if (fields.question.trim().length < 5) {
+      valid = false;
+      errors.question = "Question must be at least 5 characters long";
+    } else {
+      errors.question = "";
+    }
+
+    if (fields.answerA.trim().length < 1) {
+      valid = false;
+      errors.answerA = "Answer A cannot be empty";
+    } else {
+      errors.answerA = "";
+    }
+
+    if (fields.answerB.trim().length < 1) {
+      valid = false;
+      errors.answerB = "Answer B cannot be empty";
+    } else {
+      errors.answerB = "";
+    }
+
+    if (valid) {
+        console.log('Valid', fields)
+    }
+  };
+</script>
+
+<style>
+  form {
+    width: 400px;
+    margin: 0 auto;
+    text-align: center;
+  }
+
+  .form-field {
+    margin: 18px auto;
+  }
+
+  input {
+    width: 100%;
+    border-radius: 6px;
+  }
+
+  label {
+    margin: 10px auto;
+    text-align: left;
+  }
+
+  .error {
+      font-weight: bold;
+      font-size: 12px;
+      color: #d91b42;
+  }
+</style>
+
+<form on:submit|preventDefault={handleSubmit}>
+  <div class="form-field">
+    <label for="question">Poll Question:</label>
+    <input bind:value={fields.question} type="text" id="question" />
+    <div class="error">{errors.question}</div>
+  </div>
+
+  <div class="form-field">
+    <label for="answer-a">Answer A:</label>
+    <input bind:value={fields.answerA} type="text" id="answer-a" />
+    <div class="error">{errors.answerA}</div>
+  </div>
+
+  <div class="form-field">
+    <label for="answer-b">Answer B:</label>
+    <input bind:value={fields.answerB} type="text" id="answer-b" />
+    <div class="error">{errors.answerB}</div>
+  </div>
+
+  <Button type="secondary" flat={true}>Add Poll</Button>
+</form>
